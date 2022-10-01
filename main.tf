16af83f2b4a6ab0bfad8ac8d5ec3eee1c0aab05b
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-10-01 14:16:12 +0900

    provider分けた

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  profile = "terraform-account"
-  region = "ap-northeast-1"
-}
-
 resource "aws_vpc" "container_handson_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  profile = "terraform-account"
+  region = "ap-northeast-1"
+}

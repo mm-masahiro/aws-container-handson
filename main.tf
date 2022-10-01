dbd9cdb9e7b71d7dd8749fdd25e7dfd06ffcddc7
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-10-01 12:58:27 +0900

    fix ignore

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "terraform-account"
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "container_handson_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  instance_tenancy = "default"
+  tags = {
+    "Name" = "container_handson_vpc"
+  }
+}

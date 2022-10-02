76f7da41501477bf5a1745689280dbbedf15ac28
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-10-02 23:07:10 +0900

    set subnets

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+variable aws_vpc_id {}
+
 resource "aws_vpc" "container_handson_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -5,5 +7,29 @@ resource "aws_vpc" "container_handson_vpc" {
   instance_tenancy = "default"
   tags = {
     "Name" = "container_handson_vpc"
+  }
+}
+
+resource "aws_subnet" "application_subnet_1a" {
+  vpc_id = var.aws_vpc_id
+  availability_zone = "ap-northeast-1a"
+  cidr_block = "10.0.8.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    "Name" = "application_subnet_1a",
+    "Type" = "Isolated"
+  }
+}
+
+resource "aws_subnet" "application_subnet_1c" {
+  vpc_id = var.aws_vpc_id
+  availability_zone = "ap-northeast-1c"
+  cidr_block = "10.0.9.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    "Name" = "application_subnet_1c",
+    "Type" = "Isolated"
   }
 }
